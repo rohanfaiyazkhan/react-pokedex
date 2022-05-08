ff862097f6589abe94640fcd1fddc4918fc07fc7
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-05-07 20:31:39 -0400

    Double border added to type indicator

--- src/fragments/TypeIndicators/TypeIndicators.jsx
+++ src/fragments/TypeIndicators/TypeIndicators.jsx
@@ -11,11 +11,13 @@ const TypeIndicatorContainer = styled.div`
 const TypeIndicator = styled.div`
     background-color: var(${VarKeyMap.Light2});
     border: 4px solid var(${VarKeyMap.RedShadow});
-    width: 40%;
-    font-size: 1.2rem;
-    padding: 0.1rem 0.3rem;
+    box-shadow: 0 0 0 2px var(${VarKeyMap.Dark1});
+    font-family: "Pokemon";
+    width: 35%;
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
     height: 1.5em;
-    margin: 0 0.1rem;
+    margin: 0 0.25rem;
     border-radius: 8px;
 `;
 
